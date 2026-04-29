3eee3758733fcb2222f5b4880fd591d642ca66e4
truthixify <truthixify@gmail.com>
2026-04-29 11:15:12 +0100

    Auto saved by Logseq

--- code/nn/main.py
+++ code/nn/main.py
@@ -1,7 +1,107 @@
 import numpy as np
 import pandas as pd
-import utils
 
+def initialize_parameters(n_x):
+    """
+    Returns:
+    params -- python dictionary containing your parameters:
+                    W -- weight matrix of shape (n_y, n_x)
+                    b -- bias value set as a vector of shape (n_y, 1)
+    """
+    
+    W = np.random.randn(1, n_x) * 0.01 # @REPLACE EQUALS None
+    b = np.zeros((1, 1)) # @REPLACE EQUALS None
+    
+    assert (W.shape == (1, n_x))
+    assert (b.shape == (1, 1))
+    
+    parameters = {"W": W,
+                  "b": b}
+    
+    return parameters
+
+def compute_cost(Y_hat, Y):
+    """
+    Computes the cost function as a sum of squares
+    
+    Arguments:
+    Y_hat -- The output of the neural network of shape (n_y, number of examples)
+    Y -- "true" labels vector of shape (n_y, number of examples)
+    
+    Returns:
+    cost -- sum of squares scaled by 1/(2*number of examples)
+    
+    """
+    # Number of examples.
+    m = Y.shape[1]
+
+    # Compute the cost function.
+    cost = np.sum((Y_hat - Y)**2)/(2*m)
+    
+    return cost
+
+
+def backward_propagation(A, X, Y):
+    """
+    Implements the backward propagation, calculating gradients
+    
+    Arguments:
+    parameters -- python dictionary containing our parameters 
+    A -- the output of the neural network of shape (1, number of examples)
+    X -- input data of shape (n_x, number of examples)
+    Y -- "true" labels vector of shape (n_y, number of examples)
+    
+    Returns:
+    grads -- python dictionary containing gradients with respect to different parameters
+    """
+    m = X.shape[1]
+    
+    # Backward propagation: calculate dW, db.
+    dZ = A - Y
+    dW = 1/m * np.matmul(dZ, X.T)
+    db = 1/m * np.sum(dZ, axis = 1, keepdims = True)
+    
+    grads = {"dW": dW,
+             "db": db}
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate = 1.2):
+    """
+    Updates parameters using the gradient descent update rule
+    
+    Arguments:
+    parameters -- python dictionary containing parameters 
+    grads -- python dictionary containing gradients 
+    
+    Returns:
+    parameters -- python dictionary containing updated parameters 
+    """
+    # Retrieve each parameter from the dictionary "parameters".
+    W = parameters["W"]
+    b = parameters["b"]
+    
+    # Retrieve each gradient from the dictionary "grads".
+    dW = grads["dW"]
+    db = grads["db"]
+    
+    # Update rule for each parameter.
+    W = W - learning_rate * dW
+    b = b - learning_rate * db
+    
+    parameters = {"W": W,
+                  "b": b}
+    
+    return parameters
+
+def train_nn(parameters, A, X, Y, learning_rate = 0.01):
+    # Backpropagation. Inputs: "A, X, Y". Outputs: "grads".
+    grads = backward_propagation(A, X, Y)
+    
+    # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
+    parameters = update_parameters(parameters, grads, learning_rate)
+    
+    return parameters
 
 def forward_propagation(X, parameters):
     """
