481fd65f397d73af6c4b628c60e7daa5f7d6ddc1
truthixify <truthixify@gmail.com>
2026-05-08 11:28:05 +0100

    Auto saved by Logseq

--- code/algorithms/multilayer_model.py
+++ code/algorithms/multilayer_model.py
@@ -118,6 +118,142 @@ def forward_propagation(X, parameters):
     
     return A2, cache
 
+def backward_propagation(parameters, cache, X, Y):
+    """
+    Implements the backward propagation, calculating gradients
+    
+    Arguments:
+    parameters -- python dictionary containing our parameters 
+    cache -- python dictionary containing Z1, A1, Z2, A2
+    X -- input data of shape (n_x, number of examples)
+    Y -- "true" labels vector of shape (n_y, number of examples)
+    
+    Returns:
+    grads -- python dictionary containing gradients with respect to different parameters
+    """
+    m = X.shape[1]
+    
+    # First, retrieve W from the dictionary "parameters".
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    
+    # Retrieve also A1 and A2 from dictionary "cache".
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+    
+    # Backward propagation: calculate partial derivatives denoted as dW1, db1, dW2, db2 for simplicity. 
+    dZ2 = A2 - Y
+    dW2 = 1/m * np.dot(dZ2, A1.T)
+    db2 = 1/m * np.sum(dZ2, axis = 1, keepdims = True)
+    dZ1 = np.dot(W2.T, dZ2) * A1 * (1 - A1)
+    dW1 = 1/m * np.dot(dZ1, X.T)
+    db1 = 1/m * np.sum(dZ1, axis = 1, keepdims = True)
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
+
+def update_parameters(parameters, grads, learning_rate=1.2):
+    """
+    Updates parameters using the gradient descent update rule
+    
+    Arguments:
+    parameters -- python dictionary containing parameters 
+    grads -- python dictionary containing gradients
+    learning_rate -- learning rate for gradient descent
+    
+    Returns:
+    parameters -- python dictionary containing updated parameters 
+    """
+    # Retrieve each parameter from the dictionary "parameters".
+    W1 = parameters['W1']
+    b1 = parameters['b1']
+    W2 = parameters['W2']
+    b2 = parameters['b2']
+    
+    # Retrieve each gradient from the dictionary "grads".
+    dW1 = grads['dW1']
+    db1 = grads['db1']
+    dW2 = grads['dW2']
+    db2 = grads['db2']
+    
+    # Update rule for each parameter.
+    W1 = W1 - learning_rate * dW1
+    b1 = b1 - learning_rate * db1
+    W2 = W2 - learning_rate * dW2
+    b2 = b2 - learning_rate * db2
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+
+def nn_model(X, Y, n_h, num_iterations=10, learning_rate=1.2, print_cost=False):
+    """
+    Arguments:
+    X -- dataset of shape (n_x, number of examples)
+    Y -- labels of shape (n_y, number of examples)
+    num_iterations -- number of iterations in the loop
+    learning_rate -- learning rate parameter for gradient descent
+    print_cost -- if True, print the cost every iteration
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+    
+    # Initialize parameters.
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    
+    # Loop.
+    for i in range(0, num_iterations):
+         
+        # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+        A2, cache = forward_propagation(X, parameters)
+        
+        # Cost function. Inputs: "A2, Y". Outputs: "cost".
+        cost = compute_cost(A2, Y)
+        
+        # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
+        grads = backward_propagation(parameters, cache, X, Y)
+        
+        # Gradient descent parameter update. Inputs: "parameters, grads, learning_rate". Outputs: "parameters".
+        parameters = update_parameters(parameters, grads, learning_rate)
+        
+        # Print the cost every iteration.
+        if print_cost:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
+
+# GRADED FUNCTION: predict
+
+def predict(X, parameters):
+    """
+    Using the learned parameters, predicts a class for each example in X
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters 
+    X -- input data of size (n_x, m)
+    
+    Returns
+    predictions -- vector of predictions of our model (blue: 0 / red: 1)
+    """
+    
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A2, cache = forward_propagation(X, parameters)
+    predictions = A2 > 0.5
+    ### END CODE HERE ###
+    
+    return predictions
+
 m = 2000
 samples, labels = make_blobs(n_samples=m, 
                              centers=([2.5, 3], [6.7, 7.9], [2.1, 7.9], [7.4, 2.8]), 
